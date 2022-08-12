a99cfb52c98f2847bf846dffa015cd9de8ed83c1
leewooyup <zjadlspun1114@naver.com>
2022-08-12 20:44:07 +0900

    Answer Entity 추가

--- src/main/java/com/ll/exam/sbb/Answer.java
+++ src/main/java/com/ll/exam/sbb/Answer.java
@@ -9,16 +9,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
+    private Question question;
 }
