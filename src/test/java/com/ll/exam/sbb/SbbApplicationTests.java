f2d9b0e557d8930f3e9ab1d0f4ef3d9c029d5931
leewooyup <zjadlspun1114@naver.com>
2022-08-14 15:55:53 +0900

    assertThat 도입, 모든 질문 가져오기 케이스 추가

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -29,5 +32,14 @@ class SbbApplicationTests {
 		q2.setContent("id는 자동으로 생성되나요?");
 		q2.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q2);
+	}
+
+	@Test
+	void 모든_질문_가져오기() {
+		List<Question> all = this.questionRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		Question q = all.get(0);
+		assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
 	}
 }
