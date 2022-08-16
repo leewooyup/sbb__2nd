f7697b2afea04003d7c5fa381b89e406f1ff8909
leewooyup <zjadlspun1114@naver.com>
2022-08-16 20:31:08 +0900

    modifyArticle 구현

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -2,6 +2,7 @@ package com.ll.exam.sbb;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.PackagePrivate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -112,7 +114,12 @@ public class MainController {
         return "세션변수 %s의 값이 %s입니다".formatted(name, value);
     }
 
-    private List<Article> articles = new ArrayList<>();
+    private List<Article> articles = new ArrayList<>(
+            Arrays.asList(
+                    new Article("제목1", "내용1"),
+                    new Article("제목2", "내용2")
+            )
+    );
 
     @GetMapping("/addArticle")
     @ResponseBody
@@ -133,15 +140,34 @@ public class MainController {
                 .get();
         return article;
     }
+
+    @GetMapping("/modifyArticle/{id}")
+    @ResponseBody
+    public String modifyArticle(@PathVariable int id, String title, String body) {
+        Article article = articles
+                .stream()
+                .filter(a -> a.getId() == id)
+                .findFirst()
+                .get();
+        if(article == null) {
+            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+        }
+
+        article.setTitle(title);
+        article.setBody(body);
+
+        return "%d번 게시물을 수정하였습니다.".formatted(article.getId());
+    }
 }
 
 @AllArgsConstructor
 @Getter
+@Setter
 class Article {
     private static int lastId = 0;
-    private final int id;
-    private final String title;
-    private final String body;
+    private int id;
+    private String title;
+    private String body;
 
     public Article(String title, String body) {
         this(++lastId, title, body);
