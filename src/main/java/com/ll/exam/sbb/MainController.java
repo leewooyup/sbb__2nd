5c9a5043cb4901d692166abc9007a779be293675
leewooyup <zjadlspun1114@naver.com>
2022-08-16 20:38:24 +0900

    get() -> orElse()

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -137,7 +137,7 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
-                .get();
+                .orElse(null);
         return article;
     }
 
@@ -148,7 +148,7 @@ public class MainController {
                 .stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
-                .get();
+                .orElse(null);
         if(article == null) {
             return "%d번 게시물은 존재하지 않습니다.".formatted(id);
         }
