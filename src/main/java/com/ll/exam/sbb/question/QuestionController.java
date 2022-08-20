f4e292185cf28c703fbf0ea17c0eb510eff2099f
leewooyup <zjadlspun1114@naver.com>
2022-08-20 17:45:36 +0900

    타임리프 캐시 끄고 Model타입 변수로 값 전달

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -1,15 +1,25 @@
 package com.ll.exam.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+    private final QuestionRepository questionRepository;
     @RequestMapping("/question/list")
     //@ResponseBody
     // @RequestBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+
+        model.addAttribute("questionList", questionList);
+
         return "question_list";
     }
 }
