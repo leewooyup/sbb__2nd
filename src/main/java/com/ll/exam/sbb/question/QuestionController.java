dced2cc904f70e9837a7b16579d17156b9aec85a
leewooyup <zjadlspun1114@naver.com>
2022-08-20 18:40:13 +0900

    question_detail 구현

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.ll.exam.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,5 +22,13 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
 
         return "question_list";
+    }
+
+    @RequestMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable int id) {
+        Question question = questionService.getQuestioin(id);
+
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -11,5 +12,13 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestioin(int id) {
+        Optional<Question> oq = questionRepository.findById(id);
+        if(oq.isPresent()) {
+            return oq.get();
+        }
+        return null;
     }
 }
