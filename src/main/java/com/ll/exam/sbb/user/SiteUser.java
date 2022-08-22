76c5bfb81b7899a6b157a90b261fd8eba9543406
leewooyup <zjadlspun1114@naver.com>
2022-08-22 10:02:01 +0900

    UserRepository, UserService 추가 create 구현

--- src/main/java/com/ll/exam/sbb/user/SiteUser.java
+++ src/main/java/com/ll/exam/sbb/user/SiteUser.java
@@ -1,11 +1,13 @@
 package com.ll.exam.sbb.user;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
