e347c3973f067436532e390cb96fd9499e6679b2
leewooyup <zjadlspun1114@naver.com>
2022-08-22 14:23:48 +0900

    로그인 폼 구현

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -21,7 +21,11 @@ public class SecurityConfig {
                 .and()
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
                         XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
-                ));
+                ))
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/");
         return http.build();
     }
 
