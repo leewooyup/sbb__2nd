1bf83f720019a202bac10e008e87c7d2fee537fe
leewooyup <zjadlspun1114@naver.com>
2022-08-23 15:42:06 +0900

    스프링 시큐리티를 통한 로그인 폼 처리

---
+++ src/main/java/com/ll/exam/sbb/user/UserRole.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sbb.user;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+    private String value;
+    UserRole(String value) {
+        this.value = value;
+    }
+}
