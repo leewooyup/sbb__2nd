827bdc33befe8faafe0f45ec00b480dd6ac927ff
leewooyup <zjadlspun1114@naver.com>
2022-08-25 14:31:30 +0900

    질문 수정 폼, 질문 등록 폼을 재활용

--- src/main/java/com/ll/exam/sbb/question/QuestionForm.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionForm.java
@@ -2,11 +2,12 @@ package com.ll.exam.sbb.question;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
-@AllArgsConstructor
+@Setter
 @Getter
 public class QuestionForm {
     @NotEmpty(message = "제목은 필수항목입니다.")
