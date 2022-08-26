8a273d8d1483b90a5dfe0aa1b55e78eb9062ba25
leewooyup <zjadlspun1114@naver.com>
2022-08-26 11:30:02 +0900

    질문 추천 기능 구현

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -44,4 +44,9 @@ public class QuestionService {
     public void delete(Question question) {
         this.questionRepository.delete(question);
     }
+
+    public void vote(Question question, SiteUser siteUser) {
+        question.getVoter().add(siteUser);
+        questionRepository.save(question);
+    }
 }
