1339dd69a62c4321d323379a77e5b03be3f0a68f
leewooyup <zjadlspun1114@naver.com>
2022-08-28 11:05:52 +0900

    답변작성 및 추천시 해당 위치로 스크롤 이동

--- src/main/java/com/ll/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerController.java
@@ -44,8 +44,8 @@ public class AnswerController {
 
             SiteUser siteUser = userService.getUser(principal.getName());
 
-            answerService.create(question, answerForm.getContent(), siteUser);
-            return "redirect:/question/detail/%d".formatted(id);
+            Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
+            return "redirect:/question/detail/%d#answer_%d".formatted(id, answer.getId());
         }
 
         throw new DataNotFoundException("question not found");
@@ -77,7 +77,7 @@ public class AnswerController {
         }
         answerService.modify(answer, answerForm.getContent());
 
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -101,6 +101,6 @@ public class AnswerController {
         SiteUser siteUser = userService.getUser(principal.getName());
 
         answerService.vote(answer, siteUser);
-        return "redirect:/question/detail/%d".formatted(answer.getQuestion().getId());
+        return "redirect:/question/detail/%d#answer_%d".formatted(answer.getQuestion().getId(), answer.getId());
     }
 }
--- src/main/java/com/ll/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerService.java
@@ -13,13 +13,15 @@ import java.time.LocalDateTime;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author) {
+    public Answer create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setAuthor(author);
         question.addAnswer(answer);
         answerRepository.save(answer);
+
+        return answer;
     }
 
     public Answer getAnswer(Integer id) {
